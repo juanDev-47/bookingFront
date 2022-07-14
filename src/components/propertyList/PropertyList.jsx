9c5667e5ee27a7b5e4473cede1708fc037534eca
juanDev-47 <juanp.arenas@udea.edu.co>
2022-07-13 22:23:49 -0500

    propertyList almost finished

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./propertyList.css";
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt="img"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt="img"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt="img"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt="img"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt="img"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+      <div className="pListItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+          alt="img"
+          className="pListImg"
+        />
+        <div className="pListTitles">
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Feature from '../../components/featured/Feature';
 import { Header } from '../../components/header/Header';
 import { Navbar } from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
 import "../home/home.css";
 
 export const Home = () => {
@@ -8,6 +10,11 @@ export const Home = () => {
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Feature />
+        <h1 className='homeTitle'>Browse by property type</h1>
+        <PropertyList /> 
+      </div>
     </div>
   )
 }
